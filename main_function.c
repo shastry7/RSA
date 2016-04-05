c9e0a7d68fa19e9b1a24c92071849bffd49cc666
shastry7 <apoorva7shastry@gmail.com>
2016-04-05 19:06:48 +0530

    Update main_function.c

--- main_function.c
+++ main_function.c
@@ -1,126 +1,98 @@
-
 #include<GL/glut.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include<GL/gl.h>
 #include<string.h>
-
 #define		mainmenu		100
 
 int flag=0;
-
 char E[1000];
-
 
 GLfloat x[25];
 GLfloat y[20];
 GLint i,j,k;
 int w,h,a,b,c,e,d,l;
-
-
-
-
 void *currentfont;
-
 void setFont(void *font)
 {
 	currentfont=font;
 }
 
-
 void drawstring(float x,float y,float z,char *string) //to display text messages
 {
-	    char *c;
-	glRasterPos3f(x,y,z);
-
-	for(c=string;*c!='\0';c++)
-	{	glColor3f(0.0,0.0,0.0);
-		glutBitmapCharacter(currentfont,*c);
-	}
+	  char *c;
+		glRasterPos3f(x,y,z);
+		for(c=string;*c!='\0';c++)
+		{
+			glColor3f(0.0,0.0,0.0);
+			glutBitmapCharacter(currentfont,*c);
+		}
 }
-
-
-
-
 
 void title()												// to draw the starting screen
 {
 
-	 setFont(GLUT_BITMAP_HELVETICA_18);
-		  glBegin(GL_POLYGON);
-          glColor3f(0.0,0.1,0.2);
-		         glVertex2i(0,500);
-			    glColor3f(0.0,0.0,0.0);
-		        glVertex2i(0,0);
-		      glColor3f(0.0,0.0,0.0);
-		        glVertex2i(500,0);
-          glColor3f(0.0,0.1,0.2);
-		        glVertex2i(500,500);
+	 	setFont(GLUT_BITMAP_HELVETICA_18);
+		glBegin(GL_POLYGON);
+          glColor3f(0.0,0.1,0.3);
+		      glVertex2i(0,500);
+			    glColor3f(0.0,0.5,0.6);
+		      glVertex2i(0,0);
+		      glColor3f(0.0,0.5,0.6);
+		      glVertex2i(500,0);
+          glColor3f(0.0,0.1,0.3);
+		      glVertex2i(500,500);
 	    glEnd();
 
-	glColor3f(1.0,0.0,1.0);
-	drawstring(20.0,435.0,1.0,"Topic name");/* Make color changes here*/
-
-	glColor3f(1.0,1.0,1.0);
-	drawstring(210.0,365.0,1.0,"submitted by");
-
-	glColor3f(0.0,1.0,1.0);
-	drawstring(180.0,340.0,1.0,"Apoorva ");
-
-	glColor3f(0.0,1.0,1.0);
-	drawstring(180.0,320.0,1.0,"Dara Sravya");
-
-
-	glColor3f(1.0,1.0,1.0);
-	drawstring(400.0,100.0,1.0," Press M -> continue");
-
-	glFlush();
+			glColor3f(1.0,1.0,1.0);
+			drawstring(20.0,435.0,1.0,"     RIVEST SHAMIR ADLEMAN ALGORITHM VISUALIZATION      ");
+			glColor3f(1.0,1.0,1.0);
+			drawstring(210.0,365.0,1.0,"		SUBMITTED BY	");
+			glColor3f(1.0,1.0,1.0);
+			drawstring(180.0,340.0,1.0,"APOORVA SHASTRY 1PE13CS030");
+			glColor3f(1.0,1.0,1.0);
+			drawstring(180.0,320.0,1.0,"DARA SRAVYA\t\t\t\t\t\t\t\t\t\t\t\t1PE13CS050");
+			glColor3f(1.0,1.0,1.0);
+			drawstring(400.0,100.0,1.0," Press M -> continue");
+			glFlush();
 }
 
 void draw(int c) // TO DRAW POLYGON FOR DISPLAY MENUS
 {
-/*Color Change here*/
-    glBegin(GL_POLYGON);
-		glColor3f(0.0f,0.0f,0.0f);
-		glVertex2i(0,425-c);
-		glColor3f(0.6076,0.0,1.6076);
-		glVertex2i(100,395-c);
-		glColor3f(0.0f,0.0f,0.0f);
-		glVertex2i(425,395-c);
-		glColor3f(0.0f,0.0f,0.0f);
-		glVertex2i(425,425-c);
-		glEnd();
-	glFlush();
+    	glBegin(GL_POLYGON);
+				glColor3f(0.0,0.4,0.2);
+				glVertex2i(0,425-c);
+				glColor3f(0.0,0.4,0.2);
+				glVertex2i(100,395-c);
+				glColor3f(0.0,0.3,0.2);
+				glVertex2i(425,395-c);
+				glColor3f(0.0,0.2,0.3);
+				glVertex2i(425,425-c);
+			glEnd();
+			glFlush();
 }
-void drawop(int d,int e) // TO DRAW POLYGON FOR selection a,b,c,detcMENUS
+void text(void)
 {
-/*Color change here*/
-    glBegin(GL_POLYGON);
-			glColor3f(0.2f,0.2f,0.7f);
-			glVertex2i(d+57.5,320-e);
-			glColor3f(0.0f,0.0f,0.0f);
-			glVertex2i(d+147.5,320-e);
-			glColor3f(0.0f,0.0f,0.0f);
-			glVertex2i(d+147.5,300-e);
-			glColor3f(0.2f,0.2f,0.7f);
-			glVertex2i(d+57.5,300-e);
-		glEnd();
-		glFlush();
-}
-
-void text(void)											// to draw the text in menu screen
-{
-
 	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_POLYGON);
+				glColor3f(0.0,0.1,0.3);
+				glVertex2i(0,500);
+				glColor3f(0.0,0.5,0.6);
+				glVertex2i(0,0);
+				glColor3f(0.0,0.5,0.6);
+				glVertex2i(500,0);
+				glColor3f(0.0,0.1,0.3);
+				glVertex2i(500,500);
+		glEnd();
 	draw(0);
 	draw(55);
 	draw(110);
 	draw(165);
 	draw(220);
-/*Color change here*/
+
 	setFont(GLUT_BITMAP_HELVETICA_18);
-	glColor3f(0.4932,0.9996,0.0);
-	drawstring(152.0,455.0,1.0,"RSA ALGORITHM");
+	glColor3f(0.5,1.0,1.0);
+	drawstring(152.0,455.0,1.0,"\t\t\t\t\t\tRSA ALGORITHM");
 
 	glColor3f(1.0,1.0,1.0);
 	drawstring(185.0,405.0,1.0,"Press K: Key generation");
@@ -135,12 +107,9 @@ void text(void)											// to draw the text in menu screen
 	drawstring(185.0,240.0,1.0,"Press D :Decryption");
 
 	glColor3f(1.0,1.0,1.0);
-	drawstring(185.0,185.0,1.0,"Press X: EXIT");
+	drawstring(185.0,185.0,1.0,"Press X: Exit");
 	glFlush();
 }
-
-
-
 
 void menuSelect(int value)
 {
@@ -149,16 +118,12 @@ void menuSelect(int value)
 	  case mainmenu:
       text();
 	  break;
-
-  }
-
-
+	}
 }
 
 
 void mykeyboard(unsigned char key,int x,int y)
 {
-
 	switch(key)
 	{
 		case 'x':
@@ -203,8 +168,7 @@ void mykeyboard(unsigned char key,int x,int y)
 		case 'M':flag=1;glutPostRedisplay();break;
 		default:return;
 	}
-
-	//glutPostRedisplay();
+	glutPostRedisplay();
 }
 
 
@@ -217,6 +181,7 @@ void myInit()
 			glMatrixMode(GL_PROJECTION);
 			glLoadIdentity();
 }
+
 void display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -224,33 +189,25 @@ void display(void)
 	{
 		title();
 	}
-
-	if(flag==1) {text();}
+	if(flag==1)
+	{
+		text();
+	}
 
 }
 
 int main(int argc, char ** argv)
 {
-	   int fillmenu = 0;
-     printf("\n Enter a message less than ten characters\n");
-     gets(E);
-		if(strlen(E)>10)
-	   {
-		      printf("Message length exceeded \n");
-		      return -1;
-	     }
-
-
-       glutInit(&argc,argv);
-       glutInitDisplayMode(GLUT_SINGLE|GLUT_RGBA);
-       glutInitWindowPosition(0,0);
-       glutInitWindowSize(1000,1000);
-       glutCreateWindow("RSA ");
-       glutKeyboardFunc(mykeyboard);
-       glutDisplayFunc(display);
-       myInit();
-
-       glutAttachMenu(GLUT_RIGHT_BUTTON);
-       glutMainLoop();
-
+     	printf("\nEnter a message less than ten characters\n");
+     	scanf("%s",E);
+			glutInit(&argc,argv);
+      glutInitDisplayMode(GLUT_SINGLE|GLUT_RGBA);
+      glutInitWindowPosition(0,0);
+      glutInitWindowSize(1000,1000);
+      glutCreateWindow("RSA ");
+      glutKeyboardFunc(mykeyboard);
+      glutDisplayFunc(display);
+      myInit();
+			glutAttachMenu(GLUT_RIGHT_BUTTON);
+      glutMainLoop();
 }
